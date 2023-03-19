3e117630e30b5ee2e4c7e34a753112167917bd71
PolinaT <timoninapa@gmail.com>
2023-03-19 15:51:08 +0300

    исправления по замечаниям)

--- js/show-modal-massages.js
+++ js/show-modal-massages.js
@@ -1,4 +1,4 @@
-const VIEV_COMMENTS_COUNT = 5;
+const VIEW_COMMENTS_COUNT = 5;
 const usersComments = document.querySelector('.social__comments').children;
 
 const modalWindowShowCommentsCount = document.querySelector('.social__comment-count');
@@ -16,29 +16,25 @@ const showCommentsCount = () => {
 
   modalWindowShowCommentsCount.firstChild.data = `${usersCommentsShows} из `;
 
-  if (usersCommentsShows === usersComments.length) {
-    modalWindowShowCommentsButton.classList.add('hidden');
-  } else {
-    modalWindowShowCommentsButton.classList.remove('hidden');
-  }
+  modalWindowShowCommentsButton.classList.toggle('hidden', usersCommentsShows === usersComments.length);
 };
 
 const showFirstComments = () => {
-  for (let i = usersComments.length - 1; i >= VIEV_COMMENTS_COUNT; i--) {
+  for (let i = usersComments.length - 1; i >= VIEW_COMMENTS_COUNT; i--) {
     usersComments[i].classList.add('hidden');
   }
   showCommentsCount();
 };
 
-const showNewCountComments = (vievCommentsCount) => {
-  const beforeVievCommentsCount = vievCommentsCount - VIEV_COMMENTS_COUNT - 1;
+const showNewCountComments = (viewCommentsCount) => {
+  const beforeviewCommentsCount = viewCommentsCount - VIEW_COMMENTS_COUNT - 1;
 
-  if (usersComments.length < vievCommentsCount) {
-    for (let i = beforeVievCommentsCount; i < usersComments.length; i++) {
+  if (usersComments.length < viewCommentsCount) {
+    for (let i = beforeviewCommentsCount; i < usersComments.length; i++) {
       usersComments[i].classList.remove('hidden');
     }
   } else {
-    for (let i = beforeVievCommentsCount; i < vievCommentsCount; i++) {
+    for (let i = beforeviewCommentsCount; i < viewCommentsCount; i++) {
       usersComments[i].classList.remove('hidden');
     }
   }
@@ -47,9 +43,9 @@ const showNewCountComments = (vievCommentsCount) => {
 };
 
 modalWindowShowCommentsButton.addEventListener('click', () => {
-  let vievCommentsCount = VIEV_COMMENTS_COUNT;
-  vievCommentsCount += VIEV_COMMENTS_COUNT;
-  showNewCountComments(vievCommentsCount);
+  let viewCommentsCount = VIEW_COMMENTS_COUNT;
+  viewCommentsCount += VIEW_COMMENTS_COUNT;
+  showNewCountComments(viewCommentsCount);
 });
 
 
