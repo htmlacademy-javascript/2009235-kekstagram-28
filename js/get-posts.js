c7bb6528ab1cb0f15000ed6b8d7dec79c09ad9f1
PolinaT <timoninapa@gmail.com>
2023-03-27 23:42:49 +0300

    Все работает, но ничего не понятно =)

--- js/get-posts.js
+++ js/get-posts.js
@@ -1,27 +1,13 @@
-//import {getData} from './api.js';
-//import {showAlert} from './utils.js';
-
-async function getPostsData () {
-  const response = await fetch('https://28.javascript.pages.academy/kekstagram/data');
-  if (!response.ok) {
-    throw new Error(`${response.status} — ${response.statusText}`);
-  }
-  return response.json();
-}
-
-/*import {simularPictures} from './add-pictures.js';
 import {getData} from './api.js';
 import {showAlert} from './utils.js';
 
-getData()
-  .then((postsData) => {
-    simularPictures(postsData);
-  })
-  .catch(
-    (err) => {
-      showAlert(err.message);
-    }
-  );
-  */
+async function getPostsData () {
+  try {
+    const response = await getData();
+    return response;
+  } catch (err) {
+    showAlert(err.message);
+  }
+}
 
 export {getPostsData};
