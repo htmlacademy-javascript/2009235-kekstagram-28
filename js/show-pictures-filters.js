9ce59a1642de6326f5eacdb65272f25caf168296
PolinaT <timoninapa@gmail.com>
2023-03-30 12:44:04 +0300

    устранение дребезга

--- js/show-pictures-filters.js
+++ js/show-pictures-filters.js
@@ -1,5 +1,6 @@
 import {startSort} from './sort-pictures.js';
 import {findModalOpenElements} from './picture-modal.js';
+import {debounce} from './utils.js';
 
 const picturesFilters = document.querySelector('.img-filters');
 const pictureFiltersButtons = document.querySelectorAll('.img-filters__button');
@@ -12,14 +13,17 @@ if (document.querySelector('.picture')) {
 
 const removeActiveClass = () => pictureFiltersButtons.forEach((pictureFiltersButton) => pictureFiltersButton.classList.remove('img-filters__button--active'));
 
-function listenPictureFiltersButtons (pictureFiltersButton) {
+const listenPictureFiltersButtons = (pictureFiltersButton) => {
   pictureFiltersButton.addEventListener('click', (evt) => {
     removeActiveClass();
     evt.target.classList.add('img-filters__button--active');
-    startSort(evt.target.id);
-    findModalOpenElements();
+    const debouncedSort = debounce(() => {
+      startSort(evt.target.id);
+      findModalOpenElements();
+    });
+    debouncedSort();
   });
-}
+};
 
 for (let i = 0; i < pictureFiltersButtons.length; i++) {
   listenPictureFiltersButtons(pictureFiltersButtons[i]);
