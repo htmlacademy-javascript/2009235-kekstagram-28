4c87a9dac2a092bdbc3f0d7d0286760ff0c89dd6
PolinaT <timoninapa@gmail.com>
2023-03-31 13:12:29 +0300

    убраны лишние модули из main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,2 @@
 import './get-posts.js';
 import './user-picture-modal.js';
-import './user-picture-upload-form-check.js';
-import './user-picture-set-picture-size.js';
-import './user-picture-set-picture-filter.js';
